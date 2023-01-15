3fe827b1ae3ee638f69f5e69b4d5d5ded61603d4
Matthew Pocock <turingatemyhamster@gmail.com>
2023-01-15 17:13:52 +0000

    First cut at board rendering implemented.

--- src/e2/images.rs
+++ src/e2/images.rs
@@ -1,10 +1,11 @@
 //! Work with image representations of boards.
 //! 
-use image::Rgba;
+use image::{Rgba, GenericImage};
 use image::imageops::{rotate90, rotate180, rotate270};
 use lazy_static::lazy_static;
 use image::{self, load_from_memory, DynamicImage, GenericImageView, ImageBuffer, imageops::overlay};
 
+use crate::board::Board;
 use crate::{e2::E2_EDGE_COUNT, board::{Tile, Side::*}};
 
 use super::E2Edge;
@@ -85,14 +86,31 @@ lazy_static! {
 }
 
 /// Render a tile as an image.
-pub fn edge_image(tile: Tile<E2Edge>) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
-    let mut img = ImageBuffer::new(IMAGES[0].dimensions().0, IMAGES[0].dimensions().1);
-    
-    overlay(&mut img, &IMAGES[tile[North] as usize], 0, 0);
-    overlay(&mut img, &rotate90 (&IMAGES[tile[East]  as usize]), 0, 0);
-    overlay(&mut img, &rotate180(&IMAGES[tile[South] as usize]), 0, 0);
-    overlay(&mut img, &rotate270(&IMAGES[tile[West]  as usize]), 0, 0);
+pub fn edge_image<I: GenericImage<Pixel = Rgba<u8>>>(img: &mut I, tile: &Tile<E2Edge>) {
+    overlay(img, &IMAGES[tile[North] as usize], 0, 0);
+    overlay(img, &rotate90 (&IMAGES[tile[East]  as usize]), 0, 0);
+    overlay(img, &rotate180(&IMAGES[tile[South] as usize]), 0, 0);
+    overlay(img, &rotate270(&IMAGES[tile[West]  as usize]), 0, 0);
+}
+
+/// Render a board as an image.
+pub fn board_image(board: &Board<E2Edge>) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+    let (tile_w, tile_h) = IMAGES[0].dimensions();
+    let board_w = tile_w * board.cols as u32;
+    let board_h = tile_h * board.rows as u32;
+
+    let mut img = ImageBuffer::new(board_w, board_h);
+
+    for r in 0..board.rows {
+        for c in 0..board.cols {
+            if let Some(t) = &board[(c, r)] {
+                let c = c as u32;
+                let r = r as u32;
+                let mut sub_image = img.sub_image(c*tile_w, r*tile_h, tile_w, tile_h);
+                edge_image(&mut *sub_image, t);
+            }
+        }
+    }
 
     img
 }
-
